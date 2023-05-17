98f3ff6bb6002e32839ed01481f0cb8f4a22e24c
James Hibbeard <jhibbeard@gmail.com>
2023-05-17 13:50:39 +0200

    Style Sheets added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="section">
+          <form className="d-flex">
+            <input
+              type="text"
+              placeholder="Todo list"
+              className="form-control"
+            />
+            <button type="submit" className="btn btn-primary ms-1">
+              Add
+            </button>
+          </form>
+          <div className="list-section">
+            <h3>My Todos</h3>
+            <hr />
+            <div>
+              <div className="todoItem">
+                <input type="checkbox" />
+                <p className="ms-2">Learn React</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
